ada746f5e38b9c221b81b0e012b1c60d0696a48b
Yassine Nidkouchi <ynidkouc@e3r9p18.1337.ma>
2024-01-14 19:03:49 +0100

    fix sytax_checker

--- mandatory/syntax_checker/syntax_checker.c
+++ mandatory/syntax_checker/syntax_checker.c
@@ -6,7 +6,7 @@
 /*   By: ynidkouc <ynidkouc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/13 14:59:31 by ynidkouc          #+#    #+#             */
-/*   Updated: 2024/01/14 18:24:21 by ynidkouc         ###   ########.fr       */
+/*   Updated: 2024/01/14 19:01:45 by ynidkouc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,10 +33,19 @@ static int	syntax_par(t_token *token)
 		return (1);
 }
 
-static int	check_pipe_or_and_open_par(t_token *token)
+static int	check_pipe_or(t_token *token)
 {
 	if (token->preveus == NULL)
 		return (1);
+	if (token->next == NULL || token->next->type == AND
+		|| token->next->type == OR || token->next->type == PIPE
+		|| token->next->type == CLOSE_PAR)
+		return (1);
+	return (0);
+}
+
+static int	check_open_par(t_token *token)
+{
 	if (token->next == NULL || token->next->type == AND
 		|| token->next->type == OR || token->next->type == PIPE
 		|| token->next->type == CLOSE_PAR)
@@ -48,6 +57,8 @@ static int	check_close_par(t_token *token)
 {
 	if (token->preveus == NULL)
 		return (1);
+	if (token->next == NULL)
+		return (0);
 	if (token->next->type == OPEN_PAR || token->next->type == WORD)
 		return (1);
 	return (0);
@@ -58,6 +69,15 @@ static int	check_all_red(t_token *token)
 	if (token->next == NULL)
 		return (1);
 	if (token->next->type != WORD)
+		return (1);
+	return (0);
+}
+
+static int	check_word(t_token *token)
+{
+	if (token->next == NULL)
+		return (0);
+	if (token->next->type == OPEN_PAR)
 		return (1);
 	return (0);
 }
@@ -73,13 +93,16 @@ int	syntax_checker(t_token *token)
 		return (1);
 	while (token)
 	{
-		if (token->type == PIPE || token->type == OR || token->type == AND
-			|| token->type == OPEN_PAR)
-			is_valid = check_pipe_or_and_open_par(token);
+		if (token->type == PIPE || token->type == OR || token->type == AND)
+			is_valid = check_pipe_or(token);
+		else if (token->type == OPEN_PAR)
+			is_valid = check_open_par(token);
 		else if (token->type == CLOSE_PAR)
 			is_valid = check_close_par(token);
 		else if (token->type != WORD)
 			is_valid = check_all_red(token);
+		else if (token->type == WORD)
+			is_valid = check_word(token);
 		if (is_valid)
 			return (1);
 		token = token->next;
